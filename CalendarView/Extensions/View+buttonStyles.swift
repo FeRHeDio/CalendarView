ee12777809bdbd79fa954b3eb45b94665ed115a2
Fernando Putallaz <ferhedio@gmail.com>
2025-03-12 21:30:53 -0300

    Exntension on view to apply visual style to buttons

---
+++ CalendarView/Extensions/View+buttonStyles.swift
@@ -0,0 +1,72 @@
+//
+//  View+buttonStyles.swift
+//  CalendarView
+//
+//  Created by Fernando Putallaz on 12/03/2025.
+//
+
+import SwiftUI
+
+// Extension to apply the button styles to the calendar view
+extension View {
+    func applyActionButtonStyle(
+        backgroundColor: Color = .white,
+        foregroundColor: Color = .black,
+        shadowRadius: CGFloat = 4,
+        shadowOpacity: Double = 0.1,
+        cornerRadius: CGFloat = 8,
+        padding: EdgeInsets = EdgeInsets(top: 12, leading: 12, bottom: 12, trailing: 12)
+    ) -> some View {
+        self.buttonStyle(ActionButtonStyle(
+            backgroundColor: backgroundColor,
+            foregroundColor: foregroundColor,
+            shadowRadius: shadowRadius,
+            shadowOpacity: shadowOpacity,
+            cornerRadius: cornerRadius,
+            padding: padding
+        ))
+    }
+}
+extension View {
+    func applyIconButtonStyle(
+        backgroundColor: Color = .white,
+        foregroundColor: Color = .black,
+        shadowRadius: CGFloat = 4,
+        shadowOpacity: Double = 0.1,
+        cornerRadius: CGFloat = 8,
+        padding: EdgeInsets = EdgeInsets(top: 12, leading: 12, bottom: 12, trailing: 12)
+    ) -> some View {
+        self.buttonStyle(IconButtonStyle(
+            backgroundColor: backgroundColor,
+            foregroundColor: foregroundColor,
+            shadowRadius: shadowRadius,
+            shadowOpacity: shadowOpacity,
+            cornerRadius: cornerRadius,
+            padding: padding
+        ))
+    }
+}
+
+extension View {
+    func applyNavigationButtonStyle(
+        backgroundColor: Color = .white,
+        foregroundColor: Color = .black,
+        shadowRadius: CGFloat = 4,
+        shadowOpacity: Double = 0.1,
+        cornerRadius: CGFloat = 8,
+        padding: EdgeInsets = EdgeInsets(top: 12, leading: 12, bottom: 12, trailing: 12),
+        isGrouped: Bool = false,
+        position: NavigationButtonStyle.GroupPosition = .single
+    ) -> some View {
+        self.buttonStyle(NavigationButtonStyle(
+            backgroundColor: backgroundColor,
+            foregroundColor: foregroundColor,
+            shadowRadius: shadowRadius,
+            shadowOpacity: shadowOpacity,
+            cornerRadius: cornerRadius,
+            padding: padding,
+            isGrouped: isGrouped,
+            position: position
+        ))
+    }
+}
